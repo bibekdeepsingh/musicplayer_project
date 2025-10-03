cff92caf4c020683f084e7e9fac7338ff916c170
Bibekdeep Singh <bibekdeepsingh@rrc.ca>
2025-10-02 22:02:41 -0500

    Added all files for the assignment.

--- music-player-organizer/src/App.tsx
+++ music-player-organizer/src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
 import { PlaylistManager } from './components/PlaylistManager/PlaylistManager'
 import SubscriptionManager from './components/SubscriptionManager/SubscriptionManager'
+import { Login } from './components/login/Login'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
 
 function App() {
+ 
+  return (
+    <>
+      <div>
+        <Header/>
+        <Login/>
+        <SubscriptionManager/>
+        <PlaylistManager/>
+        <Footer/>
       </div>
     </>
   )
