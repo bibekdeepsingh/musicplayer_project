d8d1d69f618cdfcd8a3aedf818e73e3084deea46
navpreetkaur4 <navu5393@icloud.com>
2025-11-21 10:33:03 -0600

    app is working

--- Full-Stack Project/Backend/src/server.ts
+++ Full-Stack Project/Backend/src/server.ts
@@ -8,7 +8,13 @@ const app = express();
 const PORT = 3000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Routes
